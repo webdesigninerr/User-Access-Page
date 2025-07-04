a5033d984b270701da36904fea95364504569493
webdesigninerr <96651380+webdesigninerr@users.noreply.github.com>
2025-07-05 03:06:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import LoginSignup from './components/LoginSignup/LoginSignup.jsx'
+
+function App() {
+  return (
+    <div>
+     <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
